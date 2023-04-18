a998b9e7fed25f66a7d4c8721a5dd89f7ceacdc3
JordanJoseph <jordant.joseph@outlook.com>
2023-04-18 16:56:06 -0600

    create home.jsx and import to app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
