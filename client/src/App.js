994b705f5ca4a89fea8b392f8c24c0a4073fc1e2
JordanJoseph <jordant.joseph@outlook.com>
2023-05-08 12:41:50 -0600

    install MUI, create NavBar component with mui styling component, import into App and place outside of routes to maintain on page, define user in state

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
+import React, {useState} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
+import NavBar from './components/NavBar';
 
 
 function App() {
 
+  const [user, setUser] = useState('')
+
   return (
     <Router>
+      <NavBar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='*' element={<NotFound/>} />
