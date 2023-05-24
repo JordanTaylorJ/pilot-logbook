b8a27885fda1ecc7694b683ac57384af41727e5b
JordanJoseph <jordant.joseph@outlook.com>
2023-05-24 13:22:38 -0600

    create Signup component and add to routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import Home from './components/Home';
 import NotFound from './components/NotFound';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
+import Signup from './components/Signup';
 import { UserProvider } from './context/user';
+
 
 function App() {
   
@@ -19,6 +21,7 @@ function App() {
         <Route path='/' element={<Home />}/>
         <Route path='*' element={<NotFound/>} />
         <Route path='/login' element={<Login />} /> 
+        <Route path='/signup' element={<Signup/>} /> 
       </Routes>
     </Router>
     </UserProvider>
---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Signup = () => {
+
+    return(
+        <h1>Signup form here</h1>
+    )
+}
+
+export default Signup; 
